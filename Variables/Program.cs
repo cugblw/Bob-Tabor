f2a02ca25fce23032b9207444bda3f9531c38838
李伟 <cugblw2014@outlook.com>
2020-03-29 22:40:51 +0800

    Data Types and Variables

---
+++ Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            x = 7;
+            y = x + 3;
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
